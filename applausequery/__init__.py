9f53fcbcbd4e6278a865919ce7324060a69605ea
Christian Dersch <christian.dersch@physik.uni-marburg.de>
2019-09-04 18:23:52 +0200

    small fix

--- applausequery/__init__.py
+++ applausequery/__init__.py
@@ -10,4 +10,12 @@ from . import tap
 # Third party modules
 
 def auth_token(token):
+    """
+    Allows to set the APPLAUSE API token for authorization
+
+    Parameters
+    ----------
+    token : str
+        API token as obtained from APPLAUSE web interface, set to None to unset token.
+    """
     tap.token = token
